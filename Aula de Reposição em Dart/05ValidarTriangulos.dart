cc9a873fb8e13fa5ef3c7eda51ccc7e0545d22ce
leandrocode <leandroalves.web@gmail.com>
2021-10-03 18:18:04 -0300

    identificar se é um triangulo
    Inserido um if para identificar se é um triangulo, caso tenha entrada de zeros e/ou números negativos

--- Aula de Reposição em Dart/05ValidarTriangulos.dart
+++ Aula de Reposição em Dart/05ValidarTriangulos.dart
@@ -1,15 +1,15 @@
-void main(){
-  validarTriangulo(3, 5, 2);
-  
+void main() {
+  validarTriangulo(1, 5, 6);
 }
-void validarTriangulo(int lado1, int lado2, int lado3){
-  
-  if(lado1 == lado2 && lado1 == lado3){
+
+void validarTriangulo(int lado1, int lado2, int lado3) {
+  if (lado1 <= 0 || lado2 <= 0 || lado3 <= 0) {
+    print("Não é um triângulo");
+  } else if (lado1 == lado2 && lado1 == lado3) {
     print("É um triângulo equilátero");
-  
-  }else if(lado1 != lado2 && lado2 != lado3 && lado1 != lado3){
+  } else if (lado1 != lado2 && lado2 != lado3 && lado1 != lado3) {
     print("É um triângulo escaleno");
-  }else{
+  } else {
     print("É um triãngulo isóceles");
   }
 }
