ff702d6011225ed34248c9d2ed613aeaef26aad9
leandrocode <leandroalves.web@gmail.com>
2021-10-25 09:24:27 -0300

    Widgets
    noções de Widgets, linhas e colunas.
    Atualização do READ.ME

--- AndroidApp/flutter_application/README.md
+++ AndroidApp/flutter_application/README.md
@@ -1,16 +1,6 @@
-# flutter_application
+# primeiro app
 
-A new Flutter project.
+Um projeto em Flutter com os primeiros exemplos de aplicações da linguagem Dart;
+Testando conhecimentos básicos em aula.
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+# esse arquivo mudará ao longo do projeto
--- AndroidApp/flutter_application/lib/main.dart
+++ AndroidApp/flutter_application/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +12,33 @@ void main() {
             textDirection: TextDirection.ltr,
           ),
         ),
-        body: const Center(
-          child: Text(
-            'Body do App',
-            textDirection: TextDirection.rtl,
-          ),
+        body: Row(
+          children: <Widget>[
+            Expanded(
+              child: Text(
+                "Testando a Coluna da Esquerda",
+                textDirection: TextDirection.ltr,
+              ),
+            ),
+            Expanded(
+              child: Text(
+                "Testando a Coluna da Direita",
+                textDirection: TextDirection.ltr,
+              ),
+            ),
+            Expanded(
+              child: FittedBox(
+                fit: BoxFit.contain,
+                child: FlutterLogo(),
+              ),
+            ),
+            Expanded(
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text("Botão 1"),
+              ),
+            ),
+          ],
         ),
         bottomNavigationBar: BottomAppBar(
           child: Container(
